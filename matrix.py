87bb9a2965fbcd7b6d18e8c90aadd4dae4d5a833
THEODORDJURSTRÖM <theodor.djurstrom.5842@student.uu.se>
2024-10-07 11:38:36 +0200

    matrix class

--- matrix.py
+++ matrix.py
@@ -1,99 +1,104 @@
 
 
-def size(m):
-    return len(m), len(m[0]) # rows, cols
+
+class Matrix:
+    def __init__(self, m):
+        self.m = m
+
+    def size(self):
+        return len(self.m), len(self.m[0]) # rows, cols
+    
+    def transpose(self):
+        rs, cs = self.size()
+        return Matrix([[self.m[r][c]  for r in range(rs)] for c in range(cs)])
+    
+    def col(self, i):
+        rs, cs = self.size()
+        return [self.m[r][i] for r in range(rs)]
+    
+    def copy(self):
+        return Matrix([[x for x in r] for r in self.m])
+    
+    def flip(self):
+        return Matrix([r[::-1] for r in self.m[::-1]])
+    
+    @staticmethod
+    def unit(n):
+        return Matrix([[int(i == j)  for j in range(n)] for i in range(n)])
+    
+    def __str__(self):
+        s = ''
+        for r in self.m:
+            s += f"[{' '.join(str(x) for x in r)}]\n"
+        return s[:-1]
+    
+    def __add__(self, other):
+        ar, ac = self.size()
+        br, bc = other.size()
+        if ar != br or ac != bc:
+            raise ValueError('size(a) != size(b)')
+        return [[self.m[r][c] + other.m[r][c] for c in range(ac)] for r in range(ar)]
+    
+    def __mul__(self, other):
+        ar, ac = self.size()
+        br, bc = other.size()
+        if ac != br:
+            raise ValueError('cols(a) != rows(b)')
+        return Matrix([[dot(self.m[r], other.col(c)) for c in range(bc)] for r in range(ar)])
+    
+    def invert(self): # gauss
+        m = self.copy()
+        rs, cs = m.size()
+        if rs != cs:
+            raise ValueError('Rows != Cols')
+        n = rs
+        b = Matrix.unit(n)
+        m, b = gauss(m, b)
+        m, b = gauss(m.flip(), b.flip())
+        return b.flip()
+
+
 
 def dot(a, b):
     return sum(i * j for i, j in zip(a, b))
 
-def transpose(m):
-    rs, cs = size(m)
-    return [[m[r][c]  for r in range(rs)] for c in range(cs)]
 
-def add(a, b):
-    ar, ac = size(a)
-    br, bc = size(b)
-    if ar != br or ac != bc:
-        raise ValueError('size(a) != size(b)')
-    return [[a[r][c] + b[r][c] for c in range(ac)] for r in range(ar)]
-
-def scalar_mult(m, s):
-    return [[x * s for x in r] for r in m]
-
-def col(m, i):
-    rs, cs = size(m)
-    return [m[r][i] for r in range(rs)]
-
-def unit_matrix(n):
-    return [[int(i == j)  for j in range(n)] for i in range(n)]
-
-def print_matrix(m):
-    for r in m:
-        print(f"[{' '.join(str(x) for x in r)}]")
-
-def matmul(a, b):
-    ar, ac = size(a)
-    br, bc = size(b)
-    if ac != br:
-        raise ValueError('cols(a) != rows(b)')
-    return [[dot(a[r], col(b, c)) for c in range(bc)] for r in range(ar)]
-
-def matrix_copy(m):
-    return [[x for x in r] for r in m]
-
-def flip(m):
-    return [r[::-1] for r in m[::-1]]
 
 def gauss(m, b):
-    m = matrix_copy(m)
-    b = matrix_copy(b)
-    mr, mc = size(m)
-    br, bc = size(b)
+    m = m.copy()
+    b = b.copy()
+    mr, mc = m.size()
+    br, bc = b.size()
     if mr != br:
         raise ValueError('mr != br')
     n = min(mr, mc)
 
     for i in range(n):
         for j in range(i + 1, n):
-            s = -m[j][i] / m[i][i] # scalar
+            s = -m.m[j][i] / m.m[i][i] # scalar
             for k in range(mc):
-                m[j][k] += m[i][k] * s
+                m.m[j][k] += m.m[i][k] * s
             for k in range(bc):
-                b[j][k] += b[i][k] * s
+                b.m[j][k] += b.m[i][k] * s
     for i in range(n):
-        s = m[i][i]
+        s = m.m[i][i]
         for k in range(mc):
-            m[i][k] /= s
+            m.m[i][k] /= s
         for k in range(bc):
-            b[i][k] /= s
+            b.m[i][k] /= s
     return m, b
 
 
-def invert_matrix(m): # gauss
-    m = matrix_copy(m)
-    rs, cs = size(m)
-    if rs != cs:
-        raise ValueError('Rows != Cols')
-    n = rs
-    b = unit_matrix(n)
-    m, b = gauss(m, b)
-    m, b = gauss(flip(m), flip(b))
-    return flip(b)
 
+m = Matrix([[-2, -3, 2, -3],
+            [1, 0, -3, -2],
+            [0, 1, -1, -1],
+            [-2, 2, -1, -3]])
 
-
-m = [[1, 2, 3, -1],
-     [5, 6, 7, -2]]
-b = [[6], [11]]
-
-m, b = gauss(m, b)
-
-print_matrix(m)
-print_matrix(b)
-
-for w in range(-100, 100):
-    for z in range(-100, 100):
-        y = 4.75 + 0.75*w - 2*z
-        x = 6 + w - 3*z - 2*y
-        if x + 2*y + 3*z - w != 6 or  5*x + 6*y + 7*z - 2*w != 11:
-            print(1)
+mi = m.invert()
+b = Matrix([[1],
+            [2],
+            [3],
+            [4]])
+x = mi * b
+print(m * x)
